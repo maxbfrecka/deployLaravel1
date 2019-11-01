623145af2bf2617d828ad2d9a84e61ae77a75a06
maxbfrecka <maxbfrecka@gmail.com>
2019-11-01 00:28:07 +0000

    auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
+                <div class="title m-b-md" id="header1" >
                    Big Poppin
                 </div>
 
